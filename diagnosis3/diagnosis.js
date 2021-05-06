a93bfe098d2b231e028a0a60b9dace85c5637323
sims7854 <69890954+sims7854@users.noreply.github.com>
2021-05-06 15:14:45 +0900

    diagnosis3
    test 01

---
+++ diagnosis3/diagnosis.js
@@ -0,0 +1,171 @@
+
+        window.onload = function(){
+			function receive_get_data(param) {
+                var result = null, tmp = [];
+                var items = window.location.search.substring(1).split("&");
+                for (var i = 0; i < items.length; i++) {
+                    tmp = items[i].split("=");
+                    if (tmp[0] === param) result = decodeURIComponent(tmp[1]);
+                }
+                return result;
+            }
+            /* 성별-수분 */
+            var water = receive_get_data("Q2")
+            if (water == 0.8) {
+                var q2 = 15
+            }
+            if (water == 1) {
+                var q2 = 5
+            }
+            /* 수면시간-수분 */
+            var q5 = 10 / receive_get_data("Q5")
+            /* 자외선 차단제-수분 */
+            var q8 = 15 / receive_get_data("Q8")
+        
+            /* 나이-공통 */
+            var age = receive_get_data("Q3")
+            if (age > 60) {
+                var q3 = 5
+            }
+            if (age <= 60) {
+                var q3 = 4 
+            }
+            if (age <= 40) {
+                var q3 = 3 
+            }
+            if (age <= 20) {
+                var q3 = 2 
+            }
+            if (age <= 10) {
+                var q3 = 1 
+            }
+            if (age == 0) {
+                var q3 = 0
+            }
+            /* 피부타입-민감성 */
+            var sensitive = receive_get_data("Q4")
+            if (sensitive == 1) {
+                var sq = 20
+            }
+            if (sensitive != 1) {
+                var sq = 5
+            }
+            
+            /* 모공크기 점수 */
+            var input = receive_get_data("Q1")*2 + receive_get_data("Q2")*5 + q3*1 + receive_get_data("Q4")*1 +
+                        receive_get_data("Q5")*1 + receive_get_data("Q6")*1 + receive_get_data("Q7")*1 + receive_get_data("Q8")*1 + 
+                        receive_get_data("Q9")*1 + receive_get_data("Q10")*0 + receive_get_data("Q11")*0 + receive_get_data("Q12")*6 + 
+                        receive_get_data("Q13")*2 + receive_get_data("Q14")*2 + receive_get_data("Q15")*0+ receive_get_data("Q16")*0 ;
+
+            /* 피부톤 점수 */
+            var input2 = receive_get_data("Q1")*3 + receive_get_data("Q2")*5 + q3*1 + receive_get_data("Q4")*1 +
+                        receive_get_data("Q5")*1 + receive_get_data("Q6")*1 + receive_get_data("Q7")*3 + receive_get_data("Q8")*3 + 
+                        receive_get_data("Q9")*3 + receive_get_data("Q10")*0 + receive_get_data("Q11")*0 + receive_get_data("Q12")*3 + 
+                        receive_get_data("Q13")*0 + receive_get_data("Q14")*0 + receive_get_data("Q15")*0+ receive_get_data("Q16")*0 ;
+            /* 주름 점수 */
+            var input3 = receive_get_data("Q1")*1 + receive_get_data("Q2")*5 + q3*3 + receive_get_data("Q4")*1 +
+                        receive_get_data("Q5")*1 + receive_get_data("Q6")*0 + receive_get_data("Q7")*2 + receive_get_data("Q8")*2 + 
+                        receive_get_data("Q9")*2 + receive_get_data("Q10")*0 + receive_get_data("Q11")*0 + receive_get_data("Q12")*0 + 
+                        receive_get_data("Q13")*0 + receive_get_data("Q14")*0 + receive_get_data("Q15")*0+ receive_get_data("Q16")*7 ;
+            /* 색소침착 점수 */
+            var input4 = receive_get_data("Q1")*1 + receive_get_data("Q2")*5 + q3*1 + receive_get_data("Q4")*1 +
+                        receive_get_data("Q5")*2 + receive_get_data("Q6")*2 + receive_get_data("Q7")*5 + receive_get_data("Q8")*4 + 
+                        receive_get_data("Q9")*1 + receive_get_data("Q10")*0 + receive_get_data("Q11")*0 + receive_get_data("Q12")*0 + 
+                        receive_get_data("Q13")*0 + receive_get_data("Q14")*2 + receive_get_data("Q15")*0+ receive_get_data("Q16")*0 ;
+            /* 피지분비 점수 */
+            var input5 = receive_get_data("Q1")*2 + receive_get_data("Q2")*5 + q3*1 + receive_get_data("Q4")*1 +
+                        receive_get_data("Q5")*1 + receive_get_data("Q6")*1 + receive_get_data("Q7")*1 + receive_get_data("Q8")*1 + 
+                        receive_get_data("Q9")*1 + receive_get_data("Q10")*0 + receive_get_data("Q11")*0 + receive_get_data("Q12")*4 + 
+                        receive_get_data("Q13")*2 + receive_get_data("Q14")*4 + receive_get_data("Q15")*0+ receive_get_data("Q16")*0 ;
+            /* 민감성 점수 */
+            var input6 = receive_get_data("Q1")*1 + receive_get_data("Q2")*5 + q3*1 + sq +
+                        receive_get_data("Q5")*1 + receive_get_data("Q6")*1 + receive_get_data("Q7")*1 + receive_get_data("Q8")*1 + 
+                        receive_get_data("Q9")*1 + receive_get_data("Q10")*0 + receive_get_data("Q11")*0 + receive_get_data("Q12")*0 + 
+                        receive_get_data("Q13")*0 + receive_get_data("Q14")*0 + receive_get_data("Q15")*8+ receive_get_data("Q16")*0 ;
+            /* 수분 점수 */
+            var input7 = receive_get_data("Q1")*1 + q2 - q3 + receive_get_data("Q4")*3 +
+                        q5 - receive_get_data("Q6")*2 - receive_get_data("Q7")*2 + q8 - 
+                        receive_get_data("Q9")*2 + receive_get_data("Q10")*4 + receive_get_data("Q11")*4 + receive_get_data("Q12")*0 + 
+                        receive_get_data("Q13")*0 + receive_get_data("Q14")*0 + receive_get_data("Q15")*0+ receive_get_data("Q16")*0 ;
+            /* 유분 점수 */
+            var input8 = receive_get_data("Q1")*1 + receive_get_data("Q2")*5 + q3*3 + receive_get_data("Q4")*2 +
+                        receive_get_data("Q5")*1 + receive_get_data("Q6")*1 + receive_get_data("Q7")*1 + receive_get_data("Q8")*1 + 
+                        receive_get_data("Q9")*1 + receive_get_data("Q10")*4 + receive_get_data("Q11")*0 + receive_get_data("Q12")*4 + 
+                        receive_get_data("Q13")*3 + receive_get_data("Q14")*0 + receive_get_data("Q15")*0+ receive_get_data("Q16")*0 ;
+            
+			/* 모공크기 좌표 */
+            if (input <= 100) {
+                var po1 = 70
+                var po2 = (100 - input) * 0.70
+                var po = (po1 + " " + po2 + " ")
+            }
+            
+            /* 피부톤 좌표 */
+            if (input2 <= 100) {
+                var st1 = (input2 * 0.60621778) + 70
+                var st2 = ((100 - input2) * 0.35) + 35
+                var st = (st1 + " " + st2 + " ")
+            }
+            
+            /* 주름 좌표 */
+            if (input3 <= 100) {
+                var wr1 = (input3 * 0.60621778) + 70
+                var wr2 = ((input3) * 0.35) + 70
+                var wr = (wr1 + " " + wr2 + " ")
+            }
+            
+            /* 색소침착 좌표 */
+            if (input4 <= 100) {
+                var pi1 = 70
+                var pi2 = ((input4) * 0.70) +70
+                var pi = (pi1 + " " + pi2 + " ")
+            }
+          
+            /* 피지분비 좌표 */
+            if (input5 <= 100) {
+                var ss1 = 70 - (input5 * 0.60621778)
+                var ss2 = ((input5) * 0.35) + 70
+                var ss = (ss1 + " " + ss2 + " ")
+            }
+
+            /* 민감성 좌표 */
+            if (input6 <= 100) {
+                var se1 = 70 - (input6 * 0.60621778)
+                var se2 = ((100 - input6) * 0.35) + 35
+                var se = (se1 + " " + se2 + " ")
+            }
+           
+            if (input != null) {
+                var polygon = po + st + wr + pi + ss + se
+				document.getElementById("text").innerHTML =
+                    "모공크기 : " + input + "<br>" + "피부톤 : " + input2 + "<br>" + "주름 : " + input3 + "<br>" + "색소침착 : " + input4 + "<br>" + "피지분비 : " + input5 + "<br>" + "민감성 : " + input6 + "<br>" + "수분 : " + input7 + "<br>" + "유분 : " + input8 + "<br>" ;                   
+			}
+            var ani1 = anime({
+		        targets: '#morphing .polymorph',
+		        points: [
+		        	{ value: polygon },
+
+		        ],
+		        easing: 'easeInOutExpo',
+		        duration: 2000,
+		        loop: false,
+		        autoplay: true
+            });   
+            var ani2 = anime({
+                targets: '.list1 .water',
+                width: input7*2,
+                duration: 2000,
+                easing: 'easeInOutExpo',
+                autoplay: true
+            });
+            var ani3 = anime({
+                targets: '.list2 .oil',
+                width: input8*2,
+                duration: 2000,
+                easing: 'easeInOutExpo',
+                autoplay: true
+            });
+            
+		}
+        
+              
---
+++ diagnosis3/question.js
@@ -0,0 +1,63 @@
+$(function() {
+    $(".Q1 label").on("click", function() {
+        $(".Q1").fadeOut();
+        $(".Q4").delay(400).fadeIn();	
+    });
+    $(".Q4 label ").on("click", function() {
+        $(".Q4").fadeOut();
+        $(".Q5").delay(400).fadeIn();		
+    });
+    $(".Q5 label").on("click", function() {
+        $(".Q5").fadeOut();	
+        $(".Q6").delay(400).fadeIn();
+    });
+    $(".Q6 label").on("click", function() {
+        $(".Q6").fadeOut();	
+        $(".Q7").delay(400).fadeIn();	
+    });
+    $(".Q7 label").on("click", function() {
+        $(".Q7").fadeOut();	
+        $(".Q8").delay(400).fadeIn();
+    });
+    $(".Q8 label").on("click", function() {
+        $(".Q8").fadeOut();
+        $(".Q9").delay(400).fadeIn();	
+    });
+    $(".Q9 label").on("click", function() {
+        $(".Q9").fadeOut();	
+        $(".Q10").delay(400).fadeIn();	
+    });
+    $(".Q10 label").on("click", function() {
+        $(".Q10").fadeOut();
+        $(".Q11").delay(400).fadeIn();	
+    });
+    $(".Q11 label").on("click", function() {
+        $(".Q11").fadeOut();
+        $(".Q12").delay(400).fadeIn();	
+    });
+    $(".Q12 label").on("click", function() {
+        $(".Q12").fadeOut();	
+        $(".Q13").delay(400).fadeIn();
+    });
+    $(".Q13 label").on("click", function() {
+        $(".Q13").fadeOut();	
+        $(".Q14").delay(400).fadeIn();
+    });
+    $(".Q14 label").on("click", function() {
+        $(".Q14").fadeOut();
+        $(".Q15").delay(400).fadeIn();	
+    });
+    $(".Q15 label").on("click", function() {
+        $(".Q15").fadeOut();
+        $(".Q16").delay(400).fadeIn();
+    });
+    $(".Q16 label").on("click", function() {
+        $(".Q16").fadeOut();	
+        $(".Q2").delay(400).fadeIn();	
+    });
+    $(".Q2 label").on("click", function() {
+        $(".Q2").fadeOut();	
+        $(".Q3, .submit").delay(400).fadeIn();	
+    });
+
+});
